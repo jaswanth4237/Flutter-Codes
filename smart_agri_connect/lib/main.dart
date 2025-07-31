1be58c60bacb06145f6237f78b009ce484037a90
durga prasad <kingprasad4426@gmail.com>
2025-07-31 10:41:41 +0530

    changes update

--- smart_agri_connect/lib/main.dart
+++ smart_agri_connect/lib/main.dart
@@ -1,12 +1,17 @@
 
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_agri_connect/start_page.dart';
 
 
 void main()
 {
-  runApp(DevicePreview(builder: (context)=>Start()));
+  if (kIsWeb) {
+    runApp(DevicePreview(builder: (context)=>Start()));
+  } else {
+    runApp(Start());
+  }
 }
 class Start extends StatelessWidget {
    const Start({super.key,});
