7a40cdb0459c06c39743442d4ba4381115cba4a6
jaswanth4237 <24p35a4237@acet.ac.in>
2025-07-31 11:12:56 +0530

    Add the Portraiton up

--- smart_agri_connect/lib/main.dart
+++ smart_agri_connect/lib/main.dart
@@ -3,10 +3,17 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_agri_connect/start_page.dart';
+import 'package:flutter/services.dart';
+
 
 
 void main()
-{
+async{
+   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+   DeviceOrientation.portraitUp,
+  ]);
+
   if (kIsWeb) {
     runApp(DevicePreview(builder: (context)=>Start()));
   } else {
